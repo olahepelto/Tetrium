d639a4df992ab440dad86799777b64e4c926a5dd
Otto Lähepelto <otto.lahepelto@gmail.com>
2015-06-24 23:01:26 +0300

    Gui part of registration finally DONE!

--- registration.php
+++ registration.php
@@ -1,24 +1,11 @@
 <!DOCTYPE html>
-<script>
-if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
-window.alert("No ie allowed !")
-window.location.replace("https://www.google.com/intl/en/chrome/browser/");
-}
-else
-</script>
 <html>
 <head>
 	<link rel="shortcut icon" type="image/ico" href="/images/favicon.ico"/>
-	<title>Tetrium Login</title>
+	<title>Registration</title>
 <link rel="stylesheet" type="text/css" href="style/login-form.css" />
 </head>
-	<body>
-		
-		
-		
-		
-<?php include("includes/analytics.php");?>
-            
+<body>
     <div id="wrapper">
         <div id="headerwrap">
         <div id="header"    
@@ -28,10 +15,10 @@ else
         <div id="contentwrap">
         <div id="content">
 			
-			<p><form name="form1" method="post" action="executables/checklogin.php">
+	<p><form name="form1" method="post" action="executables/register.php">
   <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
     <tr>
-      <td width="112"><b>Login</b></td>
+      <td width="112"><b>Username</b></td>
       <td width="188"><input name="myusername" type="text" id="myusername"></td>
     </tr>
     <tr>
@@ -39,21 +26,21 @@ else
       <td><input name="mypassword" type="password" id="mypassword"></td>
     </tr>
     <tr>
-      <td>&nbsp;</td>
-      <td><input type="submit" name="Submit" value="Login"></td>
+      <td><b>Email</b></td>
+      <td><input name="myemail" type="text" id="myemail"></td>
     </tr>
     <tr>
-    <center><b><a style="color: indigo;" href="https://bitbucket.org/tetrium/tetrium/issues?status=new&status=open">Issue & Feature tracker</a></b></center>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="Submit" value="Login"></td>
     </tr>
   </table>
 </form></p>
         </div>
+            <?php if (isset($_GET["msg"])){ ?>
 	<div id="content2">
-		<?php if (!isset ($_GET["msg"]) or $_GET["msg"]==""){ ?>
-            <font size="3">Looking for beta testers!<br><a href="registration.php">Registration</a><br></font>
-			<?php }else{
-	echo $_GET["msg"]; $_GET["msg"]=""; }?></font>
+            <?php echo $_GET["msg"]; ?>
 	</div>
+            <?php }?>
         </div>
         <div id="rightcolumnwrap">
 			<?php include("includes/infocolumn.html"); ?>
