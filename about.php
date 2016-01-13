06c2fc097c0d6534770fa0fe0714274e27301653
Otto Lähepelto <otto.lahepelto@gmail.com>
2016-01-13 09:03:02 +0200

    Some small trivial changes

--- about.php
+++ about.php
@@ -1,10 +1,8 @@
-<head>	
-</head>
 <center>
 <p>This website is a game in beta.
 <p>Staff:
-<p>Otto L&auml;hepelto - CEO - Head programmer
+<p>Otto L&auml;hepelto - Head programmer
 <p>Christer S&ouml;derholm - Graphics
-<p>Dennis Svartb&auml;ck - Graphics - Design - Formulas
-<p>Niklas Svartb&auml;ck - Graphics
+<p>Dennis Svartb&auml;ck - Graphics - Game design - Formulas
+<p>Niklas Svartb&auml;ck - Village center Image
 </center>
