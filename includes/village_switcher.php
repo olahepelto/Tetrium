4dd627acab4e0c5b6c56cbd766b5efea78f42404
Prohaukka <otto.lahepelto@gmail.com>
2016-01-18 23:06:29 +0200

    Changed the way village switching works

--- includes/village_switcher.php
+++ includes/village_switcher.php
@@ -3,13 +3,15 @@
         <?php
         foreach ($all_user_villages_ids as $user_village_id) {
 
-            $result = mysql_query("SELECT village FROM map WHERE village_id='$user_village_id'");
-            while ($row = mysql_fetch_array($result)) {
-                $user_village_name = $row["village"];
+            if ($_SESSION["current_village_id"] != $user_village_id) {
+                $result = mysql_query("SELECT village FROM map WHERE village_id='$user_village_id'");
+                while ($row = mysql_fetch_array($result)) {
+                    $user_village_name = $row["village"];
+                }
+                ?>
+                <option value=<?php echo $user_village_id; ?>><?php echo $user_village_name; ?></option>
+                <?php
             }
-            ?>
-            <option value=<?php echo $user_village_id; ?>><?php echo $user_village_name; ?></option>
-            <?php
         }
         ?>
 
